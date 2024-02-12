2db30a05b82ad20de94436ef2505c4baecef7e24
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-02-12 01:11:10 -0300

    better script

--- .config/python/pyhistory.sh
+++ .config/python/pyhistory.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # Symlink to /etc/python/pythonrc
 
-if  [ ! -d "/etc/python" ]; then
+if  [ ! -f "/etc/python/pythonrc" ]; then
 	sudo mkdir /etc/python/
+	sudo ln -s $XDG_CONFIG_HOME/python/pythonrc /etc/python/pythonrc
+	echo "Link created"
+else
+	echo "Link exists already"
 fi
-sudo ln -s $XDG_CONFIG_HOME/python/pythonrc /etc/python/pythonrc
-echo "OK"
